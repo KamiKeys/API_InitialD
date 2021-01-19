427c436f43bfb8e67f945ca294dfcd7ea94eedae
Javier Amado <JavierAmado7@gmail.com>
2021-01-19 21:16:08 +0100

    Añadido Swagger a la API

--- APIWeb/Startup.cs
+++ APIWeb/Startup.cs
@@ -3,19 +3,19 @@ using APIWeb.BL.Implementations;
 using APIWeb.DAL.Models;
 using APIWeb.DAL.Repositories.Contracts;
 using APIWeb.DAL.Repositories.Implementations;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
+using Microsoft.IdentityModel.Tokens;
+using NSwag;
+using NSwag.Generation.Processors.Security;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
 
 namespace APIWeb
 {
@@ -50,6 +50,33 @@ namespace APIWeb
             services.AddScoped<IUsuarioRepository, UsuarioRepository>();
             services.AddScoped<IVehiculoBL, VehiculoBL>();
             services.AddScoped<IVehiculoRepository, VehiculoRepository>();
+
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+
+            // REGISTRAMOS SWAGGER COMO SERVICIO
+            services.AddOpenApiDocument(document =>
+            {
+                document.Title = "Título del Web API";
+                document.Description = "Descripción del Web API.";
+
+                // CONFIGURAMOS LA SEGURIDAD JWT PARA SWAGGER,
+                // PERMITE AÑADIR EL TOKEN JWT A LA CABECERA.
+                document.AddSecurity("JWT", Enumerable.Empty<string>(),
+                    new OpenApiSecurityScheme
+                    {
+                        Type = OpenApiSecuritySchemeType.ApiKey,
+                        Name = "Authorization",
+                        In = OpenApiSecurityApiKeyLocation.Header,
+                        Description = "Copia y pega el Token en el campo 'Value:' así: Bearer {Token JWT}."
+                    }
+                );
+
+                document.OperationProcessors.Add(
+                    new AspNetCoreOperationSecurityScopeProcessor("JWT"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -72,6 +99,25 @@ namespace APIWeb
             {
                 endpoints.MapControllers();
             });
+
+            // SWAGGER
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            else
+                app.UseHsts();
+
+            app.UseHttpsRedirection();
+
+            // AÑADIMOS EL MIDDLEWARE DE AUTENTICACIÓN
+            // DE USUARIOS AL PIPELINE DE ASP.NET CORE
+            app.UseAuthentication();
+
+            // AÑADIMOS EL MIDDLEWARE DE SWAGGER (NSwag)
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+
+            app.UseMvc();
+            //SWAGGER
         }
     }
 }
