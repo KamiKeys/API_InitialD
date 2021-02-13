3d07e51645af35fd287d1425a55e5ddf45126956
Javier Amado <JavierAmado7@gmail.com>
2021-02-13 20:32:46 +0100

    Configuración del proyecto para Test de integración
    Añadidos tests de Login

---
+++ APIWeb.Test/LoginTest.cs
@@ -0,0 +1,90 @@
+﻿using APIWeb.Core.Utils;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace APIWeb.Test
+{
+    public class LoginTests : IClassFixture<TestFixture<Startup>>
+    {
+        private HttpClient Client;
+
+        public LoginTests(TestFixture<Startup> fixture)
+        {
+            Client = fixture.Client;
+        }
+
+        [Fact]
+        public async Task LoginTests_LoginCorrect_ReturnsTrue()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/Login",
+                Body = new
+                {
+                    Username = "algunJefe",
+                    Password = "123"
+                }
+            };
+
+            // Act
+            var response = await Client.PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+            var value = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.True(value == "true");
+        }
+
+        [Fact]
+        public async Task LoginTests_LoginBossInCorrect_ReturnsFalse()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/Login",
+                Body = new
+                {
+                    Username = "alguienDeVentas",
+                    Password = "123"
+                }
+            };
+
+            // Act
+            var response = await Client.PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+            var value = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.True(value == "false");
+        }
+
+        [Fact]
+        public async Task LoginTests_LoginIncorrect_ReturnsFalse()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/Login",
+                Body = new
+                {
+                    Username = "alguienDeVentas",
+                    Password = "sdfghj"
+                }
+            };
+
+            // Act
+            var response = await Client.PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+            var value = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.True(value == "false");
+        }
+
+    }
+}
